f1fc7e456b9deedb5295c1644cecfd3939e4f56d
Adam Kalt <adam.kalt.1@gmail.com>
2018-04-30 20:41:24 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -2,12 +2,17 @@ class Author
 
 attr_accessor :name, :posts
 
+@@post_count = 0
+
 def initialize(name)
   @name = name
   @posts = []
 end
 
-def add_post
+def add_post(post)
+  @posts << post
+  @@post_count +=1
+  post.author = self
 end
 
 end
