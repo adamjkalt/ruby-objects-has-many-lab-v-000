0eff809208b460acc9310ce24aa259a42e422012
Adam Kalt <adam.kalt.1@gmail.com>
2018-04-30 20:45:00 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -15,11 +15,11 @@ def add_post(post)
   post.author = self
 end
 
-def add_post_by_title(name)
-    post = Song.new(name)
-    @songs << song
-    @@song_count +=1
-    song.artist = self
+def add_post_by_title(title)
+    post = author.new(title)
+    @posts << post
+    @@post_count +=1
+    post.author = self
   end
 
 end
